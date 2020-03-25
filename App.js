c33e0ebe9fb515dc4c33e3e7d608da3a0d903a31
thinkholic <mailtokasun@gmail.com>
2020-03-25 20:35:57 +0530

    Add stylings

--- App.js
+++ App.js
@@ -1,59 +1,95 @@
 import React, { useState } from 'react';
-import { StyleSheet, Button, TextInput, View } from 'react-native';
+import { StyleSheet, Button, TextInput, View, Text } from 'react-native';
 
 import { encrypt, decrypt } from './libs/xcrypt';
+
+export const ActionButton = ({ title, onPress, ...rest }) => (<View style={styles.actionButton}><Button title={title} onPress={onPress} {...rest} /></View>);
 
 export default function App() {
   const [passwordOrCipher, setPasswordOrCipher] = useState('');
   const [masterKey, setMasterKey] = useState('');
+  const [touchedEncrypt, setTouchedEncrypt] = useState(null);
+  const [touchedDecrypt, setTouchedDecrypt] = useState(null);
 
-  const _encrypt = () => setPasswordOrCipher(encrypt(passwordOrCipher, masterKey));
+  const _encrypt = () => {
+    setPasswordOrCipher(encrypt(passwordOrCipher, masterKey));
+    setTouchedEncrypt(true);
+    setTouchedDecrypt(false);
+  };
 
-  const _decrypt = () => setPasswordOrCipher(decrypt(passwordOrCipher, masterKey));
+  const _decrypt = () => {
+    setPasswordOrCipher(decrypt(passwordOrCipher, masterKey));
+    setTouchedDecrypt(true);
+    setTouchedEncrypt(false);
+  };
+
+  const _reset = () => {
+    setPasswordOrCipher('');
+    setMasterKey('');
+    setTouchedEncrypt(null);
+    setTouchedDecrypt(null);
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.passwordOrCipherContainer} >
         <TextInput
-          style={styles.passwordOrCipher}
+          style={styles.passwordOrCipherInput}
           placeholder="Password or Ciper"
           onChangeText={str => setPasswordOrCipher(str)}
           value={passwordOrCipher}
           multiline={true}
+          numberOfLines={15}
+        />
+        <TextInput
+          style={styles.masterKeyInput}
+          placeholder="Master key."
+          secureTextEntry={true}
+          onChangeText={key => setMasterKey(key)}
+          value={masterKey}
         />
       </View>
-      <TextInput
-        style={{height: 40}}
-        placeholder="Master key."
-        secureTextEntry={true}
-        onChangeText={key => setMasterKey(key)}
-        value={masterKey}
-      />
-      <Button title="Encrypt" onPress={_encrypt} />
-      <Button title="Decrypt" onPress={_decrypt} />
+
+      <View style={styles.buttonContainer}>
+        <ActionButton title="Encrypt" disabled={touchedEncrypt} onPress={_encrypt} />
+        <ActionButton title="Decrypt" disabled={touchedDecrypt} onPress={_decrypt} />
+        <ActionButton title="Reset" onPress={_reset} />
+      </View>
+
+      <View style={styles.logoContainer}>
+        <Text style={styles.textLogo}>XCRYPT</Text>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     width: '100%',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#ffffff',
+    marginTop: 25,
+    padding: 10,
   },
   passwordOrCipherContainer: {
-    borderColor: '#ccc',
-    borderWidth: 1,
-    width: '98%',
+    display: 'flex',
+    alignItems: 'stretch',
   },
-  passwordOrCipher: {
-    margin: 15,
-    height: 50,
-    width: '98%',
-    borderColor: '#7a42f4',
-    borderWidth: 1,
-    justifyContent: "flex-start",
+  passwordOrCipherInput: {
+    backgroundColor: '#e6dfe2',
+    marginBottom: 5,
+    padding: 10,
+    textAlignVertical: 'top',
   },
+  masterKeyInput: {
+    backgroundColor: '#e6dfe2',
+    marginBottom: 5,
+    padding: 10,
+  },
+  buttonContainer: {
+    display: 'flex',
+  },
+  actionButton: { marginTop: 5 },
+  logoContainer: { padding: 20 },
+  textLogo: { textAlign: 'center', color: '#ff0000', fontWeight: 'bold' }
 });
